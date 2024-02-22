e331c5c584539065df2257124734c696580f0043
hasin202 <88514256+hasin202@users.noreply.github.com>
2024-02-22 15:35:37 +0000

    feat(user-info): made user-info slice and stored account uid in local storage (#4)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
+import { getAccountUid } from "@/modules/user-info/user-info-slice";
+import { useDispatch } from "react-redux";
+import { ThunkDispatch } from "@reduxjs/toolkit";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const dispatch = useDispatch<ThunkDispatch<string, void, any>>();
+  useEffect(() => {
+    dispatch(getAccountUid());
+    const a = localStorage.getItem("accountUid");
+    console.log(a);
+  }, []);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
