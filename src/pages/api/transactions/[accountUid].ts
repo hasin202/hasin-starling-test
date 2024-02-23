f27354f212b67c309f370499aa39705923a9e1de
personal <hasinmahmood01@gmail.com>
2024-02-23 22:17:17 +0000

    chore(api): remove console.log

--- src/pages/api/transactions/[accountUid].ts
+++ src/pages/api/transactions/[accountUid].ts
@@ -44,7 +44,6 @@ export default async function handler(
     //with the request on the front end need to pass the parameter accountUid.
     //in the form /api/balance?accountUid=${accountUid}
     const { accountUid, minTimeStamp, maxTimeStamp } = req.query;
-    console.log(maxTimeStamp);
     const { data: response } = await starling<StarlingApiTransactionResponse>(
       `/feed/account/${accountUid}/settled-transactions-between?minTransactionTimestamp=${minTimeStamp}&maxTransactionTimestamp=${maxTimeStamp}`
     );
