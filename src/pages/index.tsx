c66f8ee9ba239f9fb7f61ea57094c6039324ee86
hasin202 <88514256+hasin202@users.noreply.github.com>
2024-02-23 17:12:28 +0000

    test(user-info): bring back unit tests for get acociut uid thunk (#14)
    * chore(balance): get balance from state and render it to check functionality
    
    * Restore tests for user info slice and isolate test store in a separate file to prevent state interference

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 import { getAccountUid } from "@/modules/user-info/user-info-slice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ThunkDispatch } from "@reduxjs/toolkit";
+import { RootState } from "@/redux/store";
 
 export default function Home() {
   const dispatch = useDispatch<ThunkDispatch<string, void, any>>();
+  const { balance } = useSelector((state: RootState) => state.userInfo);
 
   useEffect(() => {
     dispatch(getAccountUid());
   }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 `}
     >
       <p>hello</p>
+      <p>{JSON.stringify(balance)}</p>
     </main>
   );
 }
