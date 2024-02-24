629425216035e8164fc2014677289495d106f4b3
personal <hasinmahmood01@gmail.com>
2024-02-24 12:07:28 +0000

    feat(round-up): wrote roundup logic and stored result for transaction in state

---
+++ src/modules/transactions/helpers/transaction-helpers.ts
@@ -0,0 +1,17 @@
+import formatBalance from "@/modules/user-info/helpers/format-balance";
+import { FeedItem } from "@/pages/api/transactions/[accountUid]";
+
+export const individualRoundUp = (amount: number) => {
+  if (!amount) return 0;
+  return 100 - (amount % 100);
+};
+
+export const calculateTotalRoundUpAmount = (feedItems: FeedItem[]) => {
+  const outGoingTransactions = feedItems.filter(
+    (feedItem) => feedItem.direction === "OUT"
+  );
+  const totalAmount = outGoingTransactions.reduce((acc, cur) => {
+    return acc + individualRoundUp(cur.amount.minorUnits);
+  }, 0);
+  return totalAmount;
+};
