70b4e5518304a33d92e02af66d31f87915407515
hasin202 <88514256+hasin202@users.noreply.github.com>
2024-02-24 22:03:17 +0000

    refactor(balance) (#28)

--- src/modules/user-info/components/balance-skeleton.tsx
+++ src/modules/user-info/components/balance-skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 const BalanceSkeleton = () => {
   return (
     <div className="w-full px-2 py-4 flex flex-col items-center gap-2">
-      <h1 className="font-light text-slate-300">Your Balance</h1>
+      <Skeleton className="w-36 h-8 rounded" />
       <Skeleton className="w-48 h-12 rounded" />
     </div>
   );
