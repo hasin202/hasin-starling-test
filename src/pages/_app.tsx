e42ee8cccbda36685845c5dce8ed490ea90d7c04
personal <hasinmahmood01@gmail.com>
2024-02-26 01:16:23 +0000

    feat(nav): make nav bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { store } from "../redux/store";
 import { Provider } from "react-redux";
-import BlockingError from "@/modules/blocking-error/blocking-error";
-import Balance from "@/modules/user-info/components/balance";
-import { Toaster } from "@/components/ui/toaster";
+import Layout from "./layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <div className="flex flex-col py-16 px-12">
-        <BlockingError />
-        <Balance />
-        <Component {...pageProps} />
-      </div>
-      <Toaster />
+      <Layout Component={<Component {...pageProps} />} />
     </Provider>
   );
 }
