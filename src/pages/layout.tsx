4ff3e73d45c9fe02af5b033f24450cf131d4a669
personal <hasinmahmood01@gmail.com>
2024-02-26 14:51:18 +0000

    fix(strict-mode): make sure initial api calls are made only once. Even with strict mode

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/modules/nav/nav";
 import Balance from "@/modules/user-info/components/balance";
 import { getAccountUid } from "@/modules/user-info/user-info-slice";
 import { ThunkDispatch } from "@reduxjs/toolkit";
-import { ReactElement, useEffect } from "react";
+import { ReactElement, useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 
 type LayoutProps = {
@@ -13,9 +13,13 @@ type LayoutProps = {
 
 function Layout({ Component }: LayoutProps) {
   const dispatch = useDispatch<ThunkDispatch<string, void, any>>();
+  const [count, setCount] = useState(0);
 
+  //make sure this useEffect only runs once, even while in strict mode.
   useEffect(() => {
+    const id = setInterval(() => setCount((count) => count + 1), 1000);
     dispatch(getAccountUid());
+    return () => clearInterval(id);
   }, []);
   return (
     <div>
