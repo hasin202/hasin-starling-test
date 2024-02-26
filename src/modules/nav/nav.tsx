a12bb2b7d5ee20dab51df3cc0e0f7b8277abc51b
personal <hasinmahmood01@gmail.com>
2024-02-26 02:10:11 +0000

    chore(logo): display starling logo in nav

--- src/modules/nav/nav.tsx
+++ src/modules/nav/nav.tsx
@@ -2,6 +2,7 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const routes = [
   {
@@ -17,7 +18,9 @@ const routes = [
 const Navbar: React.FC = () => {
   return (
     <div className="w-full flex justify-between">
-      <p>logo</p>
+      <div>
+        <Image src="/logo.svg" alt="starling logo" width={30} height={30} />
+      </div>
 
       <div className="hidden md:flex gap-4 items-center">
         {routes.map((r) => (
@@ -31,8 +34,10 @@ const Navbar: React.FC = () => {
           </SheetTrigger>
           <SheetContent side="right">
             <div className="flex flex-col gap-4 items-start">
-              {routes.map((r) => (
-                <Link href={r.url}>{r.name}</Link>
+              {routes.map((r, i) => (
+                <Link key={i} href={r.url}>
+                  {r.name}
+                </Link>
               ))}
             </div>
           </SheetContent>
